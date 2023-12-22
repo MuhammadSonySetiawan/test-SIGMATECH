eff4f0afb733fb2f5562328ac3037cb4b2918912
Muhammad Sony Setiawan <132990489+MuhammadSonySetiawan@users.noreply.github.com>
2023-12-23 00:29:45 +0700

    [UPDATE]

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,10 +14,14 @@ const dummyUser = {
   
   const handleLogin = () => {
     if (username === dummyUser.username && password === dummyUser.password) {
-      axios
-        .get(`http://universities.hipolabs.com/search?country=Indonesia`)
-        .then((res) => setNamaContent(res.data))
-        .catch((err) => console.log(err));
+      //  setIsLoading(true);
+      // axios
+      //   .get(`http://universities.hipolabs.com/search?country=Indonesia`)
+      //   .then((res) => setNamaContent(res.data))
+      //   .catch((err) => console.log(err))
+      //   .finally(()=>{
+      //       isLoading(false);
+      //   })
       setLoggedIn(true);
       alert("Login successful!");
     } else {
@@ -26,13 +30,13 @@ const dummyUser = {
   };
 
    useEffect(() => {
-        // axios
-        //   .get(`http://universities.hipolabs.com/search?country=Indonesia`)
-        //   .then((res) => setNamaContent(res.data))
-        //   .catch((err) => console.log(err))
-        //   .finally(()=>{
-        //     // isLoading(false)
-        //   })
+        axios
+          .get(`http://universities.hipolabs.com/search?country=Indonesia`)
+          .then((res) => setNamaContent(res.data))
+          .catch((err) => console.log(err))
+          .finally(()=>{
+            // isLoading(false)
+          })
    }, []);
 
   const handleLogout = () => {
@@ -45,94 +49,103 @@ const dummyUser = {
     <div className="App">
       {loggedIn ? (
         <>
-          <h2>Welcome, {dummyUser.username}!</h2>
-          <button onClick={handleLogout}>Logout</button>
-          <div className="container">
-            <h1>Welcome, </h1>
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col">No.</th>
-                  <th scope="col">Name Universitas</th>
-                  <th scope="col">Website</th>
-                </tr>
-              </thead>
-              <tbody>
-                {namaContent?.map((item, key) => (
-                  <tr key={key}>
-                    <td>{key + 1}</td>
-                    <td>
-                      <button
-                        className="btn btn-link text-decoration-none text-dark"
-                        data-bs-toggle="modal"
-                        data-bs-target="#exampleModal"
-                      >
-                        {item.name}
-                      </button>
+          {/* {isLoading === true ? (
+            "Loading..."
+          ) : ( */}
+            <>
+              <h2>Welcome, {dummyUser.username}!</h2>
+              <button onClick={handleLogout}>Logout</button>
+              <div className="container">
+                <h1>Welcome, </h1>
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">No.</th>
+                      <th scope="col">Name Universitas</th>
+                      <th scope="col">Website</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {namaContent?.map((item, key) => (
+                      <tr key={key}>
+                        <td>{key + 1}</td>
+                        <td>
+                          <button
+                            className="btn btn-link text-decoration-none text-dark"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                          >
+                            {item.name}
+                          </button>
 
-                      {/*  */}
-                      {/* <!-- Button trigger modal --> */}
+                          {/*  */}
+                          {/* <!-- Button trigger modal --> */}
 
-                      {/* <!-- Modal --> */}
-                      <div
-                        class="modal fade"
-                        id="exampleModal"
-                        tabindex="-1"
-                        aria-labelledby="exampleModalLabel"
-                        aria-hidden="true"
-                      >
-                        <div class="modal-dialog">
-                          <div class="modal-content">
-                            <div class="modal-header">
-                              <h5 class="modal-title" id="exampleModalLabel">
-                                Detail
-                              </h5>
-                              <button
-                                type="button"
-                                class="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                              ></button>
-                            </div>
-                            <div class="modal-body">
-                              <table class="table">
-                                <tr>
-                                  <td>No. </td>
-                                  <td>:</td>
-                                  <td>{key + 1}</td>
-                                </tr>
-                                <tr>
-                                  <th>Nama Universitas</th>
-                                  <td>:</td>
-                                  <td> {item.name}</td>
-                                </tr>
-                                <tr>
-                                  <th>Website</th>
-                                  <td>:</td>
-                                  <td>{item.domains[0]}</td>
-                                </tr>
-                              </table>
-                            </div>
-                            <div class="modal-footer">
-                              <button
-                                type="button"
-                                class="btn btn-secondary"
-                                data-bs-dismiss="modal"
-                              >
-                                Close
-                              </button>
+                          {/* <!-- Modal --> */}
+                          <div
+                            class="modal fade"
+                            id="exampleModal"
+                            tabindex="-1"
+                            aria-labelledby="exampleModalLabel"
+                            aria-hidden="true"
+                          >
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5
+                                    class="modal-title"
+                                    id="exampleModalLabel"
+                                  >
+                                    Detail
+                                  </h5>
+                                  <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                  ></button>
+                                </div>
+                                <div class="modal-body">
+                                  <table class="table">
+                                    <tr>
+                                      <td>No. </td>
+                                      <td>:</td>
+                                      <td>{key + 1}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>Nama Universitas</th>
+                                      <td>:</td>
+                                      <td> {item.name}</td>
+                                    </tr>
+                                    <tr>
+                                      <th>Website</th>
+                                      <td>:</td>
+                                      <td>{item.domains[0]}</td>
+                                    </tr>
+                                  </table>
+                                </div>
+                                <div class="modal-footer">
+                                  <button
+                                    type="button"
+                                    class="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                  >
+                                    Close
+                                  </button>
+                                </div>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </div>
-                      {/*  */}
-                    </td>
-                    <td>{item.web_pages[0]}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                          {/*  */}
+                        </td>
+                        <td>{item.web_pages[0]}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </>
+          {/* )} */}
         </>
       ) : (
         <div className="d-flex justify-content-center align-items-center">
@@ -189,7 +202,9 @@ const dummyUser = {
               />
             </label>
             <br />
-            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogin} disabled={isLoading}>
+              Sign In
+            </button>
           </div>
         </div>
       )}
